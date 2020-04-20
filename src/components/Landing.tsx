575975b22db6c662817ee456d9d2963911c6bafc
Tristanoshier <tsoshier@bsu.edu>
2020-04-20 14:53:59 -0400

    set up react project

---
+++ src/components/Landing.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+export default function Landing() {
+    return (
+        <div className= 'landing-bg'>
+            <Navbar />
+            <div className="name-header">Tristan Oshier</div>
+            <div className="occupation-header">Junior Software Engineer</div>
+        </div>
+    )
+}
